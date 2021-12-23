f4d6163c93f1f546256f1bcbb998398c0e7c8681
Jerry Young <jerryyoung2911@gmail.com>
2021-12-23 13:19:49 +0700

    Fix Typo

--- part1/problem2/problem2.go
+++ part1/problem2/problem2.go
@@ -7,7 +7,7 @@ func MoneyChange(money int) []int {
 }
 
 func main() {
-	fmt.Println(MoneyChange(123))   //[100 20 1 1]
+	fmt.Println(MoneyChange(123))   //[100 20 1 1 1]
 	fmt.Println(MoneyChange(432))   //[200 200 20 10 1 1]
 	fmt.Println(MoneyChange(543))   //[500 20 20 1 1 1]
 	fmt.Println(MoneyChange(7752))  //[5000 2000 500 200 50 1 1]
--- part1/problem2/problem2_test.go
+++ part1/problem2/problem2_test.go
@@ -15,7 +15,7 @@ func TestMoneyChange(t *testing.T) {
 	}
 
 	arrTestCase := []TestCase{
-		{123, []int{100, 20, 1, 1}, "Hasil Tidak Sesuai"},
+		{123, []int{100, 20, 1, 1, 1}, "Hasil Tidak Sesuai"},
 		{432, []int{200, 200, 20, 10, 1, 1}, "Hasil Tidak Sesuai"},
 		{543, []int{500, 20, 20, 1, 1, 1}, "Hasil Tidak Sesuai"},
 		{7752, []int{5000, 2000, 500, 200, 50, 1, 1}, "Hasil Tidak Sesuai"},
